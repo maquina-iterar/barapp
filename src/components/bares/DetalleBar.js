b4ae46b4d3bc4676005698295cd069303dd25f42
Christian Escalante <chescalante.ar@gmail.com>
2020-09-13 20:24:02 -0300

    5ta iteración

--- src/components/bares/DetalleBar.js
+++ src/components/bares/DetalleBar.js
@@ -7,8 +7,12 @@ import Typography from "@material-ui/core/Typography";
 import Layout from "../Layout";
 import environment from "../../environment";
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useMutation, queryCache } from "react-query";
 import { useParams } from "react-router-dom";
+import IconButton from "@material-ui/core/IconButton";
+import LikeIcon from "@material-ui/icons/ThumbUp";
+import DislikeIcon from "@material-ui/icons/ThumbDown";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles({
   root: {
@@ -20,21 +24,35 @@ const useStyles = makeStyles({
 });
 
 const caracteriscasLabels = {
-  opcionVegetariana: "¿Tiene opción vegetariana?",
-  musiscaEnVivo: "¿Hay música en vivo?",
-  espacioAlAireLibre: "¿Tiene espacio al aire libre?",
+  opcionVegetariana: () => "Tiene opción vegetariana",
+  musiscaEnVivo: () => "Tiene música en vivo",
+  espacioAlAireLibre: (value) => `Tiene espacio ${value}`,
 };
+
+const queryBarDetails = "bar";
 
 const DetalleBar = () => {
   const classes = useStyles();
 
   const { id } = useParams();
 
-  const { isLoading, data: bar = {}, error } = useQuery(["bar", id], getBar);
+  const { isLoading, data: bar = {}, error } = useQuery(
+    [queryBarDetails, id],
+    getBar
+  );
 
-  console.log("bar", bar);
+  const [
+    mutateValoracion,
+    { status: valoracionStatus, error: valoracionError },
+  ] = useMutation(postValoracion, {
+    onSuccess: () => {
+      queryCache.invalidateQueries(queryBarDetails);
+    },
+  });
 
   const {
+    meGusta,
+    noMeGusta,
     nombre,
     fotoUrl,
     descripcion,
@@ -49,19 +67,54 @@ const DetalleBar = () => {
     <Layout backUrl="/">
       <Card className={classes.root}>
         <CardMedia className={classes.media} image={fotoUrl} title={nombre} />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {nombre}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {descripcion}
-          </Typography>
+        <CardContent
+          style={{ display: "flex", flexDirection: "column", gap: "20px" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <IconButton
+              color="primary"
+              disabled={["success", "loading"].includes(valoracionStatus)}
+              onClick={() => mutateValoracion({ id, valoracion: "megusta" })}
+            >
+              <LikeIcon />
+            </IconButton>
+            <Typography variant="h6" component="h6">
+              {meGusta}
+            </Typography>
+            <Divider
+              orientation="vertical"
+              flexItem
+              style={{ marginLeft: 10, marginRight: 10 }}
+            />
+            <Typography variant="h6" component="h6">
+              {noMeGusta}
+            </Typography>
+            <IconButton
+              disabled={["success", "loading"].includes(valoracionStatus)}
+              onClick={() => mutateValoracion({ id, valoracion: "nomegusta" })}
+              color="secondary"
+            >
+              <DislikeIcon />
+            </IconButton>
+          </div>
+          <div>
+            <Typography gutterBottom variant="h5" component="h2">
+              {nombre}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {descripcion}
+            </Typography>
+          </div>
           <div
             style={{
               display: "flex",
               gap: "20px",
-              marginTop: 20,
-              marginBottom: 20,
             }}
           >
             <Typography gutterBottom variant="subtitle1" component="span">
@@ -73,37 +126,53 @@ const DetalleBar = () => {
               </Typography>
             </a>
           </div>
-          <Typography gutterBottom variant="h5" component="span">
-            Características
-          </Typography>
-          {caracteristicas &&
-            Object.keys(caracteristicas).map((caracteristica) => (
-              <div style={{ display: "flex", gap: "20px", marginTop: 5 }}>
-                <Typography gutterBottom variant="subtitle1" component="span">
-                  {`${caracteriscasLabels[caracteristica]}:`}
-                </Typography>
-                <Typography gutterBottom variant="subtitle1" component="span">
-                  {formatCaracteristica(caracteristicas[caracteristica])}
-                </Typography>
-              </div>
-            ))}
-          <Typography gutterBottom variant="h5" component="span">
-            Contactos
-          </Typography>
-          {contactos &&
-            contactos.map((contacto) => (
-              <div style={{ display: "flex", gap: "20px", marginTop: 5 }}>
-                <a
-                  href={contacto.link}
-                  rel="noopener noreferrer"
-                  target="_blank"
+          <div>
+            <Typography gutterBottom variant="h5" component="span">
+              Características
+            </Typography>
+            {caracteristicas &&
+              Object.keys(caracteristicas).map((key) => (
+                <React.Fragment key={`caracteristica-${key}`}>
+                  {caracteristicas[key] && (
+                    <div style={{ display: "flex", gap: "20px", marginTop: 5 }}>
+                      <Typography
+                        gutterBottom
+                        variant="subtitle1"
+                        component="span"
+                      >
+                        {caracteriscasLabels[key](caracteristicas[key])}
+                      </Typography>
+                    </div>
+                  )}
+                </React.Fragment>
+              ))}
+          </div>
+          <div>
+            <Typography gutterBottom variant="h5" component="span">
+              Contactos
+            </Typography>
+            {contactos &&
+              contactos.map((contacto, index) => (
+                <div
+                  key={`contacto-${index}`}
+                  style={{ display: "flex", gap: "20px", marginTop: 5 }}
                 >
-                  <Typography gutterBottom variant="subtitle1" component="span">
-                    {contacto.redSocial}
-                  </Typography>
-                </a>
-              </div>
-            ))}
+                  <a
+                    href={contacto.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <Typography
+                      gutterBottom
+                      variant="subtitle1"
+                      component="span"
+                    >
+                      {formatRedSocial(contacto)}
+                    </Typography>
+                  </a>
+                </div>
+              ))}
+          </div>
         </CardContent>
       </Card>
     </Layout>
@@ -112,10 +181,14 @@ const DetalleBar = () => {
 
 export default DetalleBar;
 
-const formatCaracteristica = (value) => {
-  if (typeof value === "string") return value;
+const formatRedSocial = ({ redSocial, link }) => {
+  if (redSocial === "instagram") {
+    const linkArr = link.split("/");
 
-  return value ? "SI" : "NO";
+    return `@${linkArr[linkArr.length - 1]}`;
+  }
+
+  return redSocial;
 };
 
 const getBar = async (_, id) => {
@@ -124,4 +197,10 @@ const getBar = async (_, id) => {
   const { data } = await axios.get(apiUrl);
 
   return data;
+};
+
+const postValoracion = async ({ id, valoracion }) => {
+  const apiUrl = `${environment.apiUrl}/${id}/${valoracion}`;
+
+  return await axios.post(apiUrl);
 };
